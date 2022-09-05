55648fabdff38311377239c1361f7629b3affdaa
Nicoovied0 <briannicolasoviedo@hotmail.com>
2022-09-05 19:03:51 -0300

    get characters whit fetch

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,7 +1,31 @@
 const { Router } = require("express");
+const fetch = require('node-fetch');
 
 const router = Router();
 
 // Configurar los routers
+
+router.get('/characters', async (req, res) => {
+  try {
+    fetch('https://rickandmortyapi.com/api/character')
+      .then(response => response.json())
+      .then(data => {
+        const infoApi = data.results.map(e => {
+          return {
+            id: e.id,
+            name: e.name
+          }
+    
+        })
+        res.status(200).send(infoApi)
+      });
+    
+
+  } catch {
+    console.log('error')
+  }
+
+
+})
 
 module.exports = router;
