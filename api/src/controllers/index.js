52d88ed0432441429121d9c2ccc0308936f3c5f6
Nicoovied0 <briannicolasoviedo@hotmail.com>
2022-09-06 18:27:16 -0300

    get characters all

--- api/src/controllers/index.js
+++ api/src/controllers/index.js
@@ -1,12 +1,12 @@
-const express = require ('express')
+const express = require('express')
 const axios = require('axios');
 // const fetch = require('node-fetch');
 const { Character, Episode } = require('../db')
 
 
 
-
-// router.get('/characters', (req, res) => {
+//GET 20 PJS WITH FETCH
+// const getCharacters = async ()=> {
 //   try {
 //     fetch('https://rickandmortyapi.com/api/character')
 //       .then(response => response.json())
@@ -26,25 +26,57 @@ const { Character, Episode } = require('../db')
 //     console.log('error')
 //   }
 // })
+
+//GET 20 PJS WITH AXIOS
+// const getCharacters = async () => {
+//   const getApi = await axios('https://rickandmortyapi.com/api/character')
+//   const characterApi = getApi.data.results.map(e => {
+//     return {
+//       id: e.id,
+//       name: e.name,
+//       species: e.species,
+//       origin: e.origin.name,
+//       image: e.image,
+//     }
+//   })
+//   return characterApi
+
+// }
+
 const getCharacters = async () => {
-  const getApi = await axios('https://rickandmortyapi.com/api/character')
-  const characterApi = getApi.data.results.map(e => {
-    return {
-      id: e.id,
-      name: e.name,
-      species: e.species,
-      origin: e.origin.name,
-      image: e.image,
+  try {
+    const charactersApi = [];
+    let apiUrl = 'https://rickandmortyapi.com/api/character';
+    for (let i = 0; i <= 2; i++) {
+      let apiInfo = await axios(apiUrl)
+
+     apiInfo.data.results.forEach(e => {
+        return charactersApi.push({
+          id: e.id,
+          name: e.name,
+          species: e.species,
+          origin: e.origin.name,
+          image: e.image,
+
+        })
+      })
+      console.log(charactersApi)
+      apiUrl = apiInfo.data.info.next
+      
     }
-  })
-  return characterApi
+    return charactersApi
+
+   
+  } catch {
+    console.log("error")
+  }
 
 }
 
 
 
 
-module.exports={
+module.exports = {
   getCharacters,
   // getEpisodes,
   // getId
